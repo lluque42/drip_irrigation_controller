7e0e84325e6be17c7598eaf9622250022fd510b5
Luis Luque <luque.luis@gmail.com>
2025-08-08 20:06:08 +0200

    ADD USB capabilities for debugging
    With not much effort our project links now with the USB component
    allowing a USB serial communication for debugging (/dev/ttyACM0).

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
+// For the printf, as usual
+#include <stdio.h>
+
+// For sleep_ms() and stdio_init_all()
+// This is an umbrella header that includes:
+/*
+hardware_divider
+hardware_gpio
+hardware_uart
+pico_runtime
+pico_platform
+pico_stdio
+pico_time
+pico_util
+*/
+#include "pico/stdlib.h"
+
 int main()
 {
-	return 0;
+	stdio_init_all();
+	while (1)
+	{
+		printf("Hello world\n");
+		sleep_ms(2000);
+
+	}
 }
