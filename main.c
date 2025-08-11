3a97f21b09493a6b4e34c32d7749a06a124ce809
Luis Luque <luque.luis@gmail.com>
2025-08-11 16:20:36 +0200

    ADDed WiFi capabilities and static ip address
    More proof-of-concepts achieved regarding the network capabilities.
    I had to deep into the lwIP own documentation and source code to
    understand a couple of things and get the desired results.
    
    The generic lwipopts.h was added.
    
    Also a document for some comments on the hardware.
    
    By the way, in the real world and against all odds, I manage to
    overcome my lousy soldering skills and got the pico board sockets,
    two relays with their drive hardware parafernalia (npn transistor,
    diode, led, resistor), and connection blocks, elegantly soldered
    on a compact prototype PCB. I've still got room for one or even
    two micro switches, but I'm still deciding if using them is worthy.

--- main.c
+++ main.c
@@ -27,13 +27,19 @@ pico_util
 */
 #include "pico/stdlib.h"
 
+
+#define WIFI_CONNECT_TIMEOUT_MS 10000
+
+
+
 // For the pico_w this is the pin connected to the on-board led. It's NOT
 // a microcontroller's gpio BUT a CYW43 gpio.
 // For pico board the on-board led does connect to a RP-2040 gpio, the pin 25.
 const uint ON_BOARD_LED = CYW43_WL_GPIO_LED_PIN;
 
 // Th gpio for the electrovalve relay control
-const uint EV_REL_CTRL = 0;
+const uint EV_REL_CTRL = 22;
+const uint LIGHTS_REL_CTRL = 21;
 
 bool	on_board_led_status;
 
@@ -63,16 +69,20 @@ int main()
 
 	stdio_init_all();
 
-	cyw43_arch_init();
+	if (cyw43_arch_init())
+		while (1)
+			printf("Error setting time, I'm hanging myself!\n");
+
 	cyw43_arch_gpio_put(ON_BOARD_LED, 1);
 	on_board_led_status = 1;
 
 	gpio_init(EV_REL_CTRL);
 	gpio_set_dir(EV_REL_CTRL, GPIO_OUT);
-
-
 	gpio_put(EV_REL_CTRL, 0);
 
+	gpio_init(LIGHTS_REL_CTRL);
+	gpio_set_dir(LIGHTS_REL_CTRL, GPIO_OUT);
+	gpio_put(LIGHTS_REL_CTRL, 0);
 
 
 	time.year = 2025;
@@ -101,12 +111,77 @@ int main()
 	alarm_pattern.min = -1;
 	alarm_pattern.sec = 0;
 
-	rtc_set_alarm(&alarm_pattern, timer_alarm);
+	//rtc_set_alarm(&alarm_pattern, timer_alarm);
+
+	sleep_ms(5000);
+	// This enables the Wi-Fi in Station mode such that connections
+	// can be made to other Wi-Fi Access Points
+	cyw43_arch_enable_sta_mode();
+
+	// This connects to the wifi
+	int	result = cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID,
+			WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, WIFI_CONNECT_TIMEOUT_MS);
+	if (result != 0)
+	{
+		printf("Couldn't connect, error: %d\n", result);
+		cyw43_arch_gpio_put(ON_BOARD_LED, 0);
+		gpio_put(EV_REL_CTRL, 1);
+		gpio_put(LIGHTS_REL_CTRL, 1);
+		sleep_ms(1000);
+		
+	}
+	cyw43_arch_gpio_put(ON_BOARD_LED, 1);
+	gpio_put(EV_REL_CTRL, 0);
+	gpio_put(LIGHTS_REL_CTRL, 0);
+
+	ip_addr_t	addr;
+	ip_addr_t	mask;
+	ip_addr_t	gw;
+	struct netif	*interface;
+
+	if (!ipaddr_aton("192.168.1.250", &addr))
+	{
+		printf("Couldn't ipaddr_aton(addr)\n");
+	}
+	if (!ipaddr_aton("255.255.255.0", &mask))
+	{
+		printf("Couldn't ipaddr_aton(mask)\n");
+	}
+	if (!ipaddr_aton("192.168.1.254", &gw))
+	{
+		printf("Couldn't ipaddr_aton(gw)\n");
+	}
+	printf("%d - %d - %d\n", addr, mask, gw);
+	interface = NULL;
+	int	i = -1;
+	while (interface == NULL)
+	{
+		interface = netif_get_by_index(++i);	// Assumming lots of things here who allocates, should I free?
+		printf("Couldn't netif_get_by_index(%d)\n", i);
+	}
+	printf("The ip = %d (%s)\n", interface->ip_addr, ipaddr_ntoa(&interface->ip_addr));
+	
+	
+	netif_set_addr(interface, &addr, &mask, &gw);
 
 
 
 	while (1)
 	{
+		printf("Wakey wakey hands off snakey, world\n");
+		if (on_board_led_status)
+		{
+			cyw43_arch_gpio_put(ON_BOARD_LED, 0);
+			on_board_led_status = 0;
+			gpio_put(EV_REL_CTRL, 1);
+		}
+		else
+		{
+			cyw43_arch_gpio_put(ON_BOARD_LED, 1);
+			on_board_led_status = 1;
+			gpio_put(EV_REL_CTRL, 0);
+		}
+		sleep_ms(2000);
 //		if (!rtc_get_datetime(&time))
 //			while (1)
 //				printf("Error getting time, I'm hanging myself!\n");
@@ -124,4 +199,5 @@ int main()
 //		sleep_ms(1145);
 //
 	}
+	cyw43_arch_deinit();
 }
